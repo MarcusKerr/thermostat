21d1289389a3d1b8b30a6b8aafd84bea872f59de
Marcus <marcus.kerr94@gmail.com>
2018-08-12 13:26:10 +0100

    Added html and css

--- js/Thermostat.js
+++ js/Thermostat.js
@@ -8,7 +8,6 @@ function Thermostat() {
   this.MAX_TEMP_PSM_ON = 25;
   this.MAX_TEMP_PSM_OFF = 32;
   this.MEDIUM_ENERGY_USAGE_LIMIT =18;
-
 }
 
 Thermostat.prototype.getTemperature = function(){
@@ -60,9 +59,8 @@ Thermostat.prototype.energyUsage = function(){
   if(this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT){
     return "low-usage";
   }
-  else if(this.temperature <= this.MAX_TEMP_PSM_ON){
+  if(this.temperature <= this.MAX_TEMP_PSM_ON){
     return "medium-usage";
   }
-  else
-    return "high-usage"
+  return "high-usage"
 }
